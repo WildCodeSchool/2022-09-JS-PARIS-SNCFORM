68baf64b37b4a1e215e037e107217a5470041f70
Uriel SAUTRON <uriel.sautron@gmail.com>
2022-09-13 16:54:34 +0200

    Add routes user post put and delete

--- backend/src/router.js
+++ backend/src/router.js
@@ -14,5 +14,8 @@ router.delete("/items/:id", itemControllers.destroy);
 // * Routes User
 router.get("/users", userControllers.getAllUser);
 router.get("/users/:id", userControllers.getUser);
+router.post("/users", userControllers.addUser);
+router.put("/users/:id", userControllers.editUser);
+router.delete("/users/:id", userControllers.destroyUser);
 
 module.exports = router;
