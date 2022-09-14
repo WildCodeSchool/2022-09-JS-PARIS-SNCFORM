e928d7380aacc5f7c4b9c4e99f63877b9d2db7d2
LEGRAND Dylan (SNCF VOYAGEURS / DIRECTION DE LIGNE ALSACE / UO TRACTION DL ALSACE) <9919157t@commun.ad.sncf.fr>
2022-09-14 11:39:56 +0200

    add useNavigate and onClick navigate  to go on the about page and delete the display flex frome layout.scss

--- frontend/src/components/Button/Button.tsx
+++ frontend/src/components/Button/Button.tsx
@@ -3,11 +3,12 @@ import "./Button.scss";
 
 type ButtonType = {
   textButton: string;
+  onClick: () => void;
 };
 
-export const Button: React.FC<ButtonType> = ({ textButton }) => {
+export const Button: React.FC<ButtonType> = ({ textButton, onClick }) => {
   return (
-    <button type="button" className="button">
+    <button type="button" onClick={onClick} className="button">
       {textButton}
     </button>
   );
--- frontend/src/pages/HomePage/HomePage.tsx
+++ frontend/src/pages/HomePage/HomePage.tsx
@@ -1,21 +1,18 @@
 import React from "react";
 import "./HomePage.scss";
 import { Button } from "@components/Button/Button";
-import { HomeCarousel } from "@components/HomeCarousel/HomeCarousel";
+import { AppCarousel } from "@components/AppCarousel/AppCarousel";
 import { Link, useNavigate } from "react-router-dom";
 
 export const HomePage = () => {
   const navigate = useNavigate();
-  function handleClick() {
-    navigate("/about");
-  }
   return (
     <div className="home-page">
       <h3 className="home-page__connexion">
         <Link to="/about">Connexion</Link>
       </h3>
-      <HomeCarousel />
-      <Button textButton="Inscription" />
+      <AppCarousel />
+      <Button textButton="Inscription" onClick={() => navigate("/about")} />
     </div>
   );
 };
