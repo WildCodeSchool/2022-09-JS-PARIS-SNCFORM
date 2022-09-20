0dd9963e7ae8b339b23d7bee1f1d4121e3730a94
vanie28 <sylvanie.giraudet@sncf.fr>
2022-09-20 20:54:05 +0200

    Add new iconProfil,add mediaquery iconLink, add mediaquery & a{ text-decoration_line:none} in MenuPage, add className menu-page-header for to logout className menu-page-formation & menu-page-profil & menu-page-notifications.

--- frontend/src/pages/MenuPage/MenuPage.tsx
+++ frontend/src/pages/MenuPage/MenuPage.tsx
@@ -1,28 +1,44 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
 import { IconLink } from "../../components/IconLink/IconLink";
 import "./MenuPage.scss";
 
 export const MenuPage: React.FC = () => {
   return (
     <div className="menu-page">
-      <IconLink
-        title="Formations"
-        iconAlt="Icon de Formations"
-        icon="src\assets\images\iconFormation.jpg"
-        iconPage="########"
-      />
-      <IconLink
-        title="Profil"
-        iconAlt="Icon de Profil"
-        icon="src\assets\images\iconProfil.jpg"
-        iconPage="########"
-      />
-      <IconLink
-        title="Notifications"
-        iconAlt="Icon de Notifications"
-        icon="src\assets\images\iconNotif.png"
-        iconPage="########"
-      />
+      <div className="menu-page-formation">
+        <IconLink
+          title="Formations"
+          iconAlt="Icon de Formations"
+          icon="src\assets\images\iconFormation.jpg"
+          iconPage="########"
+        />
+      </div>
+      <div className="menu-page-profil">
+        <IconLink
+          title="Profil"
+          iconAlt="Icon de Profil"
+          icon="src\assets\images\iconProfil.jpg"
+          iconPage="########"
+        />
+      </div>
+      <div className="menu-page-notification">
+        <IconLink
+          title="Notifications"
+          iconAlt="Icon de Notifications"
+          icon="src\assets\images\iconNotif.png"
+          iconPage="########"
+        />
+      </div>
+      <div className="menu-page-header">
+        <NavLink to="######">
+          {/* pour le lien navlink to= ajouter le composant logout {Logout} */}
+          <img
+            src="src\assets\images\deconnexion.png"
+            alt="Icon de Déconnection"
+          />
+        </NavLink>
+      </div>
     </div>
   );
 };
