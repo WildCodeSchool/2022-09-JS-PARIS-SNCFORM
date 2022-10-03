211371c6174a3aa1575a6b4a3617cf8a78445007
loicWCS <loic.lebon.wcs@gmail.com>
2022-10-03 17:21:00 +0200

    add marging-bottom in field_input, modify type of manager/grade/jobType in UserSignUpType and userSchema

--- frontend/src/validations/UserValidation.ts
+++ frontend/src/validations/UserValidation.ts
@@ -4,22 +4,23 @@ const mailFormat = /^[\w-.]+@([\w-]+\.)+[\w-]{2,4}$/g;
 // exemple de mail autorisé : a-z.A-Z_123@aZ123.1234
 
 export const userSchema = yup.object().shape({
-  firstName: yup
+  firstName: yup.string().min(3, "Prénom doit contenir 3 charactères minimum"), // mettre les maximum sur tout les champs
+  lastName: yup.string().required("Nom est obligatoire"),
+  genre: yup.string().required("Civilité est un champ obligatoire"),
+  cpNumber: yup.string().required("CP est un champ obligatoire"),
+  email: yup
     .string()
-    .min(3, "votre prénom doit contenir 3 charactères minimum"),
-  lastName: yup.string().required("votre nom est obligatoire"),
-  genre: yup.string().required(),
-  cpNumber: yup.string().required(),
-  email: yup.string().matches(mailFormat).required(),
-  manager: yup.number().integer().required("selectioner votre manager"),
-  grade: yup.number().integer().required("selectioner votre grade"),
-  jobType: yup.number().integer().required("selectioner votre métier"),
+    .matches(mailFormat)
+    .required("Email est un champ obligatoire"),
+  manager: yup.string().required("Selectionner votre manager"),
+  grade: yup.string().required("Selectionner votre grade"),
+  jobType: yup.string().required("Selectionner votre métier"),
   password: yup
     .string()
     .min(6, "6 charactères minimum")
-    .required("le mot de passe est obligatoire"),
+    .required("Password est un champ obligatoire"),
   confirmPassword: yup
     .string()
     .oneOf([yup.ref("password"), null])
-    .required("les mots de passe doivent être similaires"),
+    .required("Les mots de passe doivent être similaires"),
 });
