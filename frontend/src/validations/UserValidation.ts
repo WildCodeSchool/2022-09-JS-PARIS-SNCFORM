cf81a55bb6c1ebb0bda043f85426834ac835c0ac
loicWCS <loic.lebon.wcs@gmail.com>
2022-10-05 17:03:08 +0200

    create regex for cPnumber, add min,max values to lastName, modify error message in password

--- frontend/src/validations/UserValidation.ts
+++ frontend/src/validations/UserValidation.ts
@@ -2,15 +2,23 @@ import * as yup from "yup";
 
 const mailFormat = /^[\w-.]+@([\w-]+\.)+[\w-]{2,4}$/g;
 // valid email example : a-z.A-Z_123@aZ123.1234
+const cpNumberFormat = /\b\d{7}[a-zA-Z]{1}\b/g;
 
 export const userSchema = yup.object().shape({
   firstName: yup
     .string()
     .min(3, "Prénom doit contenir 3 charactères minimum") // mettre les maximum sur tout les champs
     .required("Prénom est un champ obligatoire"),
-  lastName: yup.string().required("Nom est obligatoire"),
+  lastName: yup
+    .string()
+    .min(2, "Nom doit contenir 2 charactères minimum")
+    .max(255, "Nom doit contenir 255 charactères max")
+    .required("Nom est un champ obligatoire"),
   genre: yup.string().required("Civilité est un champ obligatoire"),
-  cpNumber: yup.string().required("CP est un champ obligatoire"),
+  cpNumber: yup
+    .string()
+    .matches(cpNumberFormat, "Cp ne correspond pas à l'identifiant Groupe SNCF")
+    .required("CP est un champ obligatoire"),
   email: yup
     .string()
     .matches(mailFormat)
@@ -21,7 +29,7 @@ export const userSchema = yup.object().shape({
   password: yup
     .string()
     .min(6, "6 charactères minimum")
-    .required("Password est un champ obligatoire"),
+    .required("Mot de passe est un champ obligatoire"),
   confirmPassword: yup
     .string()
     .oneOf([yup.ref("password"), null])
